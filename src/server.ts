cffb482b33be5a85210d5e233b3748b0eb639d6a
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-12-29 14:05:48 +0600

    Get , post query

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
-dotenv.config()
+dotenv.config();
 
-const port = 3000
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Express js , welcome!')
@@ -40,18 +40,64 @@ const initDB = async () => {
         update_at TIMESTAMP DEFAULT NOW()
         )
 
-        `)    
+        `)
 }
 
 initDB();
 
 
-app.post("/user", (req: Request, res: Response) => {
-    console.log(req.body);
-    res.status(201).json({
-        status: true,
-        message: "Api calling..."
-    })
+app.post("/user", async (req: Request, res: Response) => {
+    const { name, email, age } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO users(name,email, age) VALUES($1, $2, $3) RETURNING *`, [name, email, age]);
+        console.log(result.rows[0]);
+        res.send({
+            message: "Data insert sucessfull",
+            sucess: true,
+            data: result.rows[0]
+        })
+
+    } catch (err: any) {
+        res.status(500).json({
+            sucess: false,
+            error: err.message,
+        })
+    }
+})
+
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.send(result.rows);
+        console.log(result.rows)
+
+    } catch (err: any) {
+        res.status(500).json({
+            sucess: false,
+            error: err.message,
+        })
+    }
+})
+app.get("/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [id]);
+        if (result.rows.length === 0) {
+            res.json({
+                message: "user not found"
+            })
+        } else {
+            res.send(result.rows);
+            console.log(result.rows)
+        }
+
+    } catch (err: any) {
+        res.status(500).json({
+            sucess: false,
+            error: err.message,
+        })
+    }
 })
 
 app.listen(port, () => {
